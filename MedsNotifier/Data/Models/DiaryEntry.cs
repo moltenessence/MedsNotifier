435feb98b368baf5b2fdb06f0141deddf16a99f3
moltenessence <whatdoessence@gmail.com>
2022-06-21 18:46:33 +0300

    add diary entries model

---
+++ MedsNotifier/Data/Models/DiaryEntry.cs
@@ -0,0 +1,20 @@
+﻿using MedsNotifier.Data.CustomModelAttributes;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedsNotifier.Data.Models
+{
+    [BsonCollectionAttribute("diary_entries")]
+    [BsonIgnoreExtraElements]
+    public class DiaryEntry
+    {
+        [BsonId]
+        public Guid Id { get; set; } = new Guid();
+        public string MedicineName { get; set; }
+        public DateTime DateMedicineTaken { get; set; }
+    }
+}
